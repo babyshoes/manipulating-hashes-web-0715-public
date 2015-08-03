1e7b40190447cefa447fb8b5a49b05d2f8cc1d90
babyshoes <dianecai@gmail.com>
2015-08-03 15:43:16 -0400

    all done

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,10 +13,11 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
+  contacts["Freddy Mercury"].each do |k, v|
+    if k == :favorite_icecream_flavors
+      v.delete_if {|flavor| flavor == "strawberry"}
+    end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
-
